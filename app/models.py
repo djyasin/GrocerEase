84b39dec6a7ec780d66f48188023474ed0f64f1b
Lila <djyasin.ly@gmail.com>
2022-01-14 18:10:46 -0500

    forgot the m2m installation, installing fast.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django_toggle_m2m.toggle import ToggleManyToMany
 # Create your models here.
