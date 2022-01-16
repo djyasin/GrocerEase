eedda02807d7fb30ecb4efdc9b5919146542cbef
Lila <djyasin.ly@gmail.com>
2022-01-16 14:15:46 -0500

    Got create list functionality working. Shared screenshot of data with fe to ensure correct data is rendering and got approval to proceed.

---
+++ app/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import User, Location, Category, Product, List
+
+# class LoctaionSerializer(serializers.ModelSerializer):
+#     pass
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('category')
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ('name',
+        'image',
+        'categories',
+        'locations'       
+        )
+
+class ListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = List
+        fields = ('name',
+        'users',
+        'products'
+        )
