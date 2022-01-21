e8b4d6fbf177a033fa7604fcfa07128eed89358f
Lila <djyasin.ly@gmail.com>
2022-01-21 12:08:43 -0500

    Updated everyting to reflect change in regsiter endpoint. Got basic view list working but not totally functional.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import User, Location, Category, Product, List, Tag, ListItem
-from .serializers import ItemSerializer, ListSerializer, TagSerializer, UserSerializer, RegisterSerializer
-from rest_framework.generics import RetrieveDestroyAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView, UpdateAPIView
+from .serializers import ItemSerializer, ListSerializer, TagSerializer, UserSerializer
+from rest_framework.generics import ListAPIView, RetrieveDestroyAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView, UpdateAPIView
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.response import Response
@@ -25,11 +25,6 @@ class UpdateList(UpdateAPIView):
     queryset = List.objects.all()
     serializer_class = ListSerializer
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = (AllowAny,)
-    serializer_class = RegisterSerializer
-
 class CreateTag(generics.CreateAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
@@ -41,3 +36,16 @@ class ListItem(RetrieveUpdateDestroyAPIView):
 class CreateItem(generics.CreateAPIView):
     queryset = ListItem()
     serializer_class = ItemSerializer
+
+class Lists(generics.ListAPIView):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+
+class ViewLists(generics.ListAPIView):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = List.objects.filter(users=user.pk)
+        return queryset
