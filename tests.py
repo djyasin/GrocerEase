ec0cad32f0a45a20bed808bd3fc47d8983ceeb9e
Lila <djyasin.ly@gmail.com>
2022-03-12 14:29:45 -0500

    Added a test for user account creation

--- tests.py
+++ tests.py
@@ -9,8 +9,23 @@ from rest_framework.test import APITestCase
 from app.models import User, List, Tag, ListItem
 from app.serializers import ItemSerializer, ListSerializer, TagSerializer
 
+class CreateAccountTest(APITestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser(
+            "admin2", "admin@localhost.email", "badpassword"
+        )
+        self.client.login(username="admin2", password="badpassword")
+        self.user = User.objects.create(username="testuser")
+    def test_create_account(self):
+        data = {"username": "TestUser",
+	"password": "Badpassword"}
+        response = self.client.post("/auth/users/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
 class RegistrationTestCase(APITestCase):
     def test_registration(self):
         data = {"tag": "Bulk Shopping"}
         response = self.client.post("/grocerease/create_tag/", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
